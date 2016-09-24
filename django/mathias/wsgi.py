3083edfc5b133c44a02dd08765ef19a46fb330b2
Mateus <mateusflavio@gmail.com>
2016-09-24 15:13:41 -0300

    teste

--- django/mathias/wsgi.py
+++ django/mathias/wsgi.py
@@ -12,7 +12,7 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mathias.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mathias.settings.production")
 
 application = Cling(get_wsgi_application())
 
