c34c9e7026b34e3630ac8b92cbb29dbec2ac1378
Mateus Flavio <mateus.flavio@luizalabs.com>
2017-10-10 15:32:13 -0300

    Change query of count leaderboard in send message to slack

--- django/mathias/valeu/core/mysql_count_valeu.py
+++ django/mathias/valeu/core/mysql_count_valeu.py
@@ -7,7 +7,7 @@ def query(user_name_from):
     return """
         select count(1) as points, v.user_name_to
           from valeu v
-        where month(v.create_at) = month(now())
+        where ((month(v.create_at)  = month(now())) and (year(v.create_at) = year(now())))
         and v.user_name_to = '{user_name_from}'
           group by v.user_name_to
     """.format(**params)
